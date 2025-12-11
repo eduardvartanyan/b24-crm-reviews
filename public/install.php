400858472b8ba0d25641b3fdb1647e27cbd8d14a
evartanyan1989 <evartanyan1989@gmail.com>
2025-12-11 20:07:04 +0800

    Добавил болванку кастомного действия для бизнес-процесса

--- public/install.php
+++ public/install.php
@@ -7,8 +7,60 @@ if($result['rest_only'] === false):?>
         <script src="//api.bitrix24.com/api/v1/"></script>
         <?php if($result['install'] == true):?>
             <script>
-                BX24.init(function(){
-                    BX24.installFinish();
+                BX24.init(function() {
+                    BX24.callMethod(
+                        'bizproc.activity.add',
+                        {
+                            'CODE': 'getReviewLink',
+                            'HANDLER': 'https://crm-reviews.ru/getreviewlink.php',
+                            'AUTH_USER_ID': 96,
+                            'USE_SUBSCRIPTION': 'Y',
+                            'NAME': {
+                                'ru': 'Ссылка на отзыв'
+                            },
+                            'DESCRIPTION': {
+                                'ru': 'Действие генерирует и возвращает ссылки на отзывы для всех контактов сделки'
+                            },
+                            'PROPERTIES': {
+                                'deal_id': {
+                                    'Name': {
+                                        'ru': 'ID сделки',
+                                    },
+                                    'Description': {
+                                        'ru': '',
+                                    },
+                                    'Type': 'string',
+                                    'Required': 'Y',
+                                    'Multiple': 'N',
+                                    'Default': '{{ID}}'
+                                }
+                            },
+                            'RETURN_PROPERTIES': {
+                                'link': {
+                                    'Name': {
+                                        'ru': 'Ссылки на отзывы'
+                                    },
+                                    'Type': 'string',
+                                    'Multiple': 'Y',
+                                    'Default': null
+                                }
+                            },
+                            'DOCUMENT_TYPE': ['crm', 'CCrmDocumentDeal', 'DEAL'],
+                            'FILTER': {
+                                INCLUDE: [
+                                    ['crm', 'CCrmDocumentDeal']
+                                ]
+                            }
+                        },
+                        function(result) {
+                            if(result.error()) {
+                                alert("Error: " + result.error());
+                            } else {
+                                alert("Success: " + result.data());
+                                BX24.installFinish();
+                            }
+                        }
+                    );
                 });
             </script>
         <?php endif;?>
