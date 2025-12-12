f5be85a00d5d295c53588cafc06f117dad7f4bcf
evartanyan1989 <evartanyan1989@gmail.com>
2025-12-12 13:14:26 +0800

    Улучшил структуру проекта, установил Composer

--- src/settings.php
+++ src/settings.php
@@ -1,11 +1,4 @@
 <?php
 const C_REST_CLIENT_ID = 'app.693a9be8cbad89.38658671';
 const C_REST_CLIENT_SECRET = '5D6pEN5Oqr5i2FNI99ftI1b4tLa5HqWDWjSz5GGXp2Zr9cxWwt';
-define('C_REST_LOGS_DIR', dirname(__DIR__) . '/logs/');
-// or
-//define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
-
-//define('C_REST_CURRENT_ENCODING','windows-1251');
-//define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
-//define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
-//define('C_REST_BLOCK_LOG',true);//turn off default logs
+const C_REST_LOGS_DIR = '/var/www/html/logs/';
